d645a1dd9f5371e068b2816ec8f4e1ace6ac416f
Azusa Uchikura <azusa.uchikura@wework.com>
2018-02-14 00:39:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 
 def valid_move?(board,index)
   if position_taken?(board,index) == true
-    false
-    elsif position_taken?(board,index) == false && index.between?(0,8)
+    
+    elsif position_taken?(board,index) == true && index.between?(0,8)
     true
   end
 end
